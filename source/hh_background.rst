da88c633124a2cf6b15deab15a123eabea20899f
Keri Moyle <k.moyle@auckland.ac.nz>
2019-11-13 13:51:34 +0100

    Removed documentation rst files, added skeleton code

--- source/hh_background.rst
+++
@@ -1,9 +0,0 @@
-.. _hh_background:
-
--------------------------------------------
-Hodgkin-Huxley model background information
--------------------------------------------
-
-**TODO** Adapt the text and descriptions from
-https://tutorial-on-cellml-opencor-and-pmr.readthedocs.io/en/latest/
-as needed here
--- tutorials/tutorial0.py
+++ tutorials/tutorial0.py
@@ -3,7 +3,7 @@ import libcellml
 if __name__ == "__main__":
 
     print("--------------------------------------")
-    print("     Welcome to libCellML!")
-    print("     This version is {}".format(libcellml.versionString()))
+    print("Welcome to libCellML!")
+    print(libcellml.versionString())
     print("--------------------------------------")
 
